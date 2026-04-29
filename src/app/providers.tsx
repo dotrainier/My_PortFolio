1672ade2e68ab5e1ec4dcf01a871b1e0e6e0acad
rainierXcode <rainiersapin0131@gmail.com>
2026-04-29 17:42:23 +0800

    merge

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,15 +1,11 @@
-"use client";
+'use client';
 
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ReactNode } from "react";
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { ReactNode } from 'react';
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
   return (
-<<<<<<< HEAD
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
-=======
-    <NextThemesProvider attribute="class" defaultTheme="light" enableSystem>
->>>>>>> origin/professional
+    <NextThemesProvider attribute='class' defaultTheme='light' enableSystem>
       {children}
     </NextThemesProvider>
   );
